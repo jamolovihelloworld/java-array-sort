f12252e836659db0fc902a24ae80e28305ce7632
JAMOLOVI <jamolbekjamolov50@gmail.com>
2025-06-09 13:26:00 +0500

    Initial commit with Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 9, 1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
